33003aa38eeea9fcc3a4864496fb2f4c04d6bbd0
Ashish Dalal <ashish.dalal.cd.che22@itbhu.ac.in>
2024-06-09 02:29:20 +0530

    added the script code for second_page

---
+++ backend/api/tools/second_page.py
@@ -0,0 +1,223 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import data, basket
+
+def calc_beta(portfolio_return: pd.core.series.Series, market_return: pd.core.series.Series):
+    
+    # Find the intersection of indices
+    index = np.intersect1d(market_return.index, portfolio_return.index)
+    
+    # Filter the log returns to only include the intersected indices
+    market_return = market_return[index]
+    portfolio_return = portfolio_return[index]
+
+    # Calculate the covariance and variance
+    cov = np.cov(market_return, portfolio_return)[0, 1]
+    var = market_return.var()
+
+    # Calculate beta
+    beta = cov / var
+    
+    return beta
+
+def VaR(sorted_array, confidence_level):
+    index = round((1 - confidence_level)*len(sorted_array))
+    
+    return sorted_array[index]
+
+def CVaR(sorted_array, confidence_level):
+    index = round((1 - confidence_level)*len(sorted_array))
+    
+    avg_ret = np.mean(sorted_array[:index])
+    
+    return avg_ret
+
+def initialize(stocks_list, start_date, end_date, benchmark_ticker, market_ticker, initial_amount, num_simulations, portfolio_minimum_weights, portfolio_maximum_weights, basket_minimum_weights, basket_maximum_weights):
+    """
+    Inputs:
+        stocks_list: list of stocks
+        start_date: start date of historical data
+        end_date: end date of historical data
+        benchmark_ticker: ticker of the chosen benchmark
+        market_ticker: ticker of chosen market for the portfolio, used for beta calculation
+        initial_amount: initially invested amount
+        num_simulations: number of simulations to be performed
+        portfolio_minimum_weights: minimum weight limits for all individual stocks
+        portfolio_maximum_weights: minimum weight limits for all individual stocks
+        basket_minimum_weights: minimum weight limits for all the baskets
+        basket_maximum_weights: maximum weight limits for all the baskets
+    """
+
+    INITIAL_AMT_INVESTED = initial_amount
+    NUM_SIMULATIONS = num_simulations
+
+    ## FETCH DATA
+    stock_prices, stock_dividend = data.fetch_data(stocks_list, start_date=start_date, end_date=end_date, dividend=True)
+
+    ## CALCULATE LOG RETURNS
+    log_returns = np.log(stock_prices / stock_prices.shift(1))[1:]
+    log_returns_down_only = log_returns.applymap(lambda x: 0 if x>0 else x)
+
+    ANNUAL_TRADING_DAYS = 252
+    NUM_TRADING_DAYS = len(stock_prices)
+
+    portfolio_weights = []
+    basket_weights = []
+    annual_portfolio_returns = []
+    portfolio_risks = []
+
+    for i in range(NUM_SIMULATIONS):
+        
+        # STEP-I: GENERATING RANDOM WEIGHTS
+        w, basket_w = basket.get_adjusted_weights(
+                                            portfolio_minimum_weights=portfolio_minimum_weights,
+                                            portfolio_maximum_weights=portfolio_maximum_weights,
+                                            basket_minimum_weights=basket_minimum_weights,
+                                            basket_maximum_weights=basket_maximum_weights
+        )
+        
+        portfolio_weights.append(w)
+        basket_weights.append(basket_w)
+        
+        w = np.expand_dims(w, axis=0)
+        
+        # STEP-II: CALCULATING RETURNS
+        p_returns = (log_returns.mean() @ w.T)[0]
+        annual_portfolio_returns.append(p_returns * ANNUAL_TRADING_DAYS) # annualized portfolio return
+        
+        # STEP-III: CALCULATING RISKS
+        p_risks = np.sqrt(w @ log_returns.cov() @ w.T)[0][0]
+        portfolio_risks.append(p_risks * np.sqrt(ANNUAL_TRADING_DAYS)) # annualized portfolio risk
+
+    portfolio_weights = np.array(portfolio_weights)
+    basket_weights = np.array(basket_weights)
+    annual_portfolio_returns = np.exp(annual_portfolio_returns) - 1
+    portfolio_risks = np.array(portfolio_risks)
+
+    ## SHARPE AND OPTIMISED WEIGHTS
+
+    # risk-free rate of 10-yr bonds
+    risk_free_rate = 0.07 # annual rate
+
+    sharpe = (annual_portfolio_returns - risk_free_rate) / portfolio_risks
+
+    ## OPTIMISED WEIGHTS
+    index = np.argmax(sharpe)
+    optimised_weights = portfolio_weights[index]
+
+    ## NUM OF SHARES AND PORTFOLIO VALUE
+    num_of_shares = INITIAL_AMT_INVESTED*optimised_weights/stock_prices.iloc[0,:]
+
+    portfolio_value = np.sum(stock_prices*num_of_shares, axis=1)
+    capital_gain = (portfolio_value[-1] / portfolio_value[0] - 1)*100
+    portfolio_dividend = np.sum(stock_dividend*num_of_shares, axis=1)
+    dividend_yield = 100*np.sum(portfolio_dividend) / INITIAL_AMT_INVESTED
+
+    portfolio_log_returns = np.log(portfolio_value / portfolio_value.shift(1))[1:]
+
+    ## MARKET
+    market_prices = data.fetch_data(market_ticker, start_date=start_date, end_date=end_date)
+    market_log_returns = np.log(market_prices / market_prices.shift(1))[1:]
+
+    market_shares_bought = INITIAL_AMT_INVESTED / market_prices.iloc[0,:]
+    market_value = np.sum(market_shares_bought * market_prices, axis=1)
+
+    market_gain = (market_value[-1] / market_value[0] - 1)*100
+
+    ## SHARPE
+    sharpe_ratio = sharpe[index]
+
+    ## SORTINO
+    temp_w = np.expand_dims(optimised_weights, axis=0)
+    sortino_risk = np.sqrt(temp_w @ log_returns_down_only.cov()*ANNUAL_TRADING_DAYS @ temp_w.T)[0][0]
+    sortino_ratio = (annual_portfolio_returns[index] - risk_free_rate) / sortino_risk
+
+    ## TREYNOR
+    beta = calc_beta(portfolio_log_returns, market_log_returns.iloc[:,0])
+    treynor_ratio = (annual_portfolio_returns[index] - risk_free_rate) / beta
+
+    ## JENSON'S ALPHA
+    annual_market_return = np.exp(market_log_returns.mean()*ANNUAL_TRADING_DAYS) - 1
+    CAPM_expected_return = risk_free_rate + beta*(annual_market_return - risk_free_rate)
+    jenson_alpha = annual_portfolio_returns[index] - CAPM_expected_return
+
+
+
+    ## VaR CVaR calculation
+
+    sorted_returns = portfolio_value.pct_change()[1:]
+    sorted_returns = sorted_returns.sort_values()
+
+    ## Value-at-Risk 
+    var_at_90 = portfolio_value[-1]*VaR(sorted_returns, 0.90)
+    var_at_95 = portfolio_value[-1]*VaR(sorted_returns, 0.95)
+    var_at_99 = portfolio_value[-1]*VaR(sorted_returns, 0.99)
+
+    ## Conditional Value-at-Risk
+    cvar_at_90 = portfolio_value[-1]*CVaR(sorted_returns, 0.90)
+    cvar_at_95 = portfolio_value[-1]*CVaR(sorted_returns, 0.95)
+    cvar_at_99 = portfolio_value[-1]*CVaR(sorted_returns, 0.99)
+
+    ## VaR from Monte Carlo Method
+    probab = np.random.rand(NUM_SIMULATIONS)
+    simulated_returns = stats.norm.ppf(probab, loc=sorted_returns.mean(), scale=sorted_returns.std())
+
+    simulated_sorted_returns = np.sort(simulated_returns)
+    sim_var_at_90 = portfolio_value[-1]*CVaR(simulated_sorted_returns, 0.90)
+    sim_var_at_95 = portfolio_value[-1]*CVaR(simulated_sorted_returns, 0.95)
+    sim_var_at_99 = portfolio_value[-1]*CVaR(simulated_sorted_returns, 0.99)
+
+
+
+    ## BENCHMARK COMPARISON
+
+    benchmark_prices = data.fetch_data(benchmark_ticker, start_date=start_date, end_date=end_date)
+
+    benchmark_shares = INITIAL_AMT_INVESTED / benchmark_prices.iloc[0,:]
+    benchmark_value = np.sum(benchmark_shares * benchmark_prices, axis=1)
+    benchmark_log_returns = np.log(benchmark_value / benchmark_value.shift(1))[1:]
+
+    benchmark_gain = (benchmark_value[-1] / benchmark_value[0] - 1)*100
+
+    # Find the intersection of indices
+    index_intersect = np.intersect1d(benchmark_log_returns.index, portfolio_log_returns.index)
+
+    # Filter the log returns to only include the intersected indices
+    bm_p_log_difference = portfolio_log_returns[index_intersect] - benchmark_log_returns[index_intersect]
+
+    tracking_error_percent = bm_p_log_difference.std()*np.sqrt(ANNUAL_TRADING_DAYS)*100
+
+    information_ratio = (capital_gain - benchmark_gain)*(ANNUAL_TRADING_DAYS / NUM_TRADING_DAYS) / tracking_error_percent
+
+
+
+    ## CORRELATION MATRIX
+
+    correlation_matrix = log_returns.corr()
+
+    result_dict = {}
+    result_dict['capital_gain_%'] = capital_gain
+    result_dict['dividend_yield_%'] = dividend_yield
+    result_dict['market_gain_%'] = market_gain
+    result_dict['optimised_weights'] = optimised_weights
+    result_dict['portfolio_returns_%'] = annual_portfolio_returns
+    result_dict['portfolio_std'] = portfolio_risks[index]
+    result_dict['portfolio_beta'] = beta
+    result_dict['sharpe'] = sharpe_ratio
+    result_dict['treynor'] = treynor_ratio
+    result_dict['sortino'] = sortino_ratio
+    result_dict['jenson'] = jenson_alpha
+    result_dict['var'] = {'90%':var_at_90, '95%':var_at_95, '99%':var_at_99}
+    result_dict['cvar'] = {'90%':cvar_at_90, '95%':cvar_at_95, '99%':cvar_at_99}
+    result_dict['var_monte_carlo'] = {'90%':sim_var_at_90, '95%':sim_var_at_95, '99%':sim_var_at_99}
+    result_dict['portfolio_value'] = portfolio_value
+    result_dict['portfolio_dividend'] = portfolio_dividend
+    result_dict['var_monte_carlo_simulated_returns'] = simulated_returns
+    result_dict['benchmark_returns_%'] = benchmark_gain
+    result_dict['tracking_error_%'] = tracking_error_percent
+    result_dict['information_ratio'] = information_ratio
+    result_dict['benchmark_value'] = benchmark_value
+    result_dict['correlation_matrix'] = correlation_matrix
+
+    return result_dict
